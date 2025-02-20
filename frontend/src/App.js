9f61f524800d58d4eb42de353b49c66938719c8f
Joshua Davis <joshuad100717@outlook.com>
2025-02-19 21:11:57 -0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [result, setResult] = useState(null);
 
   const handleFormSubmit = async (data) => {
-    const response = await fetch('http://127.0.0.1:5000/prorate', {
+    const response = await fetch('canopy-six.vercel.app/prorate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
